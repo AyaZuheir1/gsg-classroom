ef05eec7a1ccd40bf1ccfb02e43d8d652bd43307
ayazuheir2@gmail.com <ayazuheir2@gmail.com>
2023-09-21 17:47:36 +0300

    commit

---
+++ app/Http/Controllers/TopicsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class TopicsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $topics = Topic::get();
+        $success = session('success');
+        return view('topics.index', compact('topics', 'success'));
+    }
+
+    // public function view()
+    // {
+    //    Method to Show Topic Details
+    //     return view('topic.view');
+    // }
+
+
+    public function create()
+    {
+        return view('topics.create');
+    }
+
+    public function add(Request $request)
+    {
+        $topic = new Topic();
+        $topic->name = $request->post('name');
+        $topic->classroom_id = $request->post('classroom_id');
+        $topic->user_id = $request->post('user_id');
+        // $topic->updated_at = date('y-m-d H:i:s');
+        // $topic->created_at = date('y-m-d H:i:s');
+        $topic->save();
+
+        return redirect(route('topic.index'));
+    }
+    public function edit($id)
+    {
+        $topic = Topic::find($id);
+        return view('topics.edit', compact('topic'));
+    }
+    public function update(Request $request, $id)
+    {
+        $topic = Topic::find($id);
+        $topic->name = $request->post('name');
+        $topic->classroom_id = $request->post('classroom_id');
+        $topic->user_id = $request->post('user_id');
+
+        $topic->save();
+
+        return redirect(route('topic.index'));
+    }
+
+    public function delete($id)
+    {
+        Topic::destroy($id);
+        return redirect(route('topic.index'));
+    }
+
+    public function trashed()
+    {
+        $topics = Topic::onlyTrashed()->latest()->get();
+
+        return view('topics.trashedTopics', compact('topics'));
+    }
+
+    public function restoreTopic($id)
+    {
+        $topic = Topic::onlyTrashed()->findOrFail($id);
+        $topic->restore();
+
+        return redirect(route('topic.index'))
+            ->with('success', "\" " . $topic->name . " \" Topic Restored Successfully");
+    }
+    public function forceDeleteTOpic($id)
+    {
+        $topic = Topic::onlyTrashed()->findOrFail($id);
+        $topic->forceDelete();
+
+        return redirect()->route('topic.index')->with('success', "\" " . $topic->name . " \" Topic Deleted Successfully");
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,127 @@
+<?php
+
+use App\Http\Controllers\Admin\TwoFactourAuthenticationController;
+use App\Http\Controllers\ClassroomController;
+use App\Http\Controllers\ClassroomPeopleController;
+use App\Http\Controllers\ClassworkController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\JoinClassroomController;
+use App\Http\Controllers\PaymentsController;
+use App\Http\Controllers\PlansController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubmissionController;
+use App\Http\Controllers\SubscriptionsController;
+use App\Http\Controllers\TopicsController;
+use App\Http\Controllers\Webhooks\StripeController;
+use App\Http\Middleware\ApplyUserPreferences;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/admin/2fa', [TwoFactourAuthenticationController::class, 'create']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('topic/view', [TopicsController::class, 'index'])->name('topic.index');
+// Route::get('topic/show',[TopicsController::class ,'show'])->name('topic.show');
+
+Route::get('topic/create', [TopicsController::class, 'create'])->name('topic.create');
+Route::post('topic/add', [TopicsController::class, 'add'])->name('topic.add');
+
+Route::get('topic/edit/{id}', [TopicsController::class, 'edit'])->name('topic.edit');
+Route::put('topic/update/{id}', [TopicsController::class, 'update'])->name('topic.update');
+
+Route::delete('/topic/delete/{id}', [TopicsController::class, 'delete'])->name('topic.delete');
+Route::group(['middleware' => ['auth'],], function () {
+    Route::prefix('/topic/trashed/')
+        ->as('topic.')
+        ->controller(TopicsController::class)
+        ->group(function () {
+            Route::get('/', 'trashed')->name('trashedTopics');
+            Route::get('/{id}', 'restoreTopic')->name('restoreTopic');
+            Route::delete('/{id}', 'forceDeleteTopic')->name('forceDeleteTopic');
+        });
+});
+ require __DIR__.'/auth.php';
+
+Route::get('plans', [PlansController::class, 'index'])
+    ->name('plans');
+
+Route::middleware(['auth:web,admin'])->group(function () {
+
+    Route::get('subscriptions/{subscription}/pay', [PaymentsController::class, 'create'])
+        ->name('checkout');
+
+    Route::post('subscriptions', [SubscriptionsController::class, 'store'])
+        ->name('subscriptions.store');
+
+    Route::post('payments', [PaymentsController::class, 'store'])
+        ->name('payments.store');
+
+    Route::get('/payments/{subscription}/success', [PaymentsController::class, 'success'])
+        ->name('payments.success');
+
+    Route::get('/payments/{subscription}/cancel', [PaymentsController::class, 'cancel'])
+        ->name('payments.cancel');
+
+    Route::prefix('/classrooms/trashed')
+        ->as('classrooms.')
+        ->controller(ClassroomController::class)
+        ->group(function () {
+            Route::get('/', 'trashed')->name('trashed');
+            Route::put('/{classroom}', 'restore')->name('restore');
+            Route::delete('/{classroom}', 'forceDelete')->name('force-delete');
+    });
+
+    Route::get('/classrooms/{classroom}/join', [JoinClassroomController::class, 'create'])
+        ->middleware('signed')
+        ->name('classrooms.join');
+
+    Route::post('/classrooms/{classroom}/join', [JoinClassroomController::class, 'store']);
+
+    Route::get('/classrooms/{classroom}/chat', [ClassroomController::class, 'chat'])->name('classrooms.chat');
+
+    Route::resources([
+        'topics' => TopicsController::class,
+        'classrooms' => ClassroomController::class
+    ]);
+
+    Route::resource('classrooms.classworks', ClassworkController::class);
+
+    Route::get('classrooms/{classroom}/people', [ClassroomPeopleController::class, 'index'])
+        ->name('classrooms.people');
+
+    Route::delete('classrooms/{classroom}/people', [ClassroomPeopleController::class, 'destroy'])
+        ->name('classrooms.people.destroy');
+
+    Route::post('comments', [CommentController::class, 'store'])
+        ->name('comments.store');
+
+    Route::post('classwork/{classwork}/submissions', [SubmissionController::class, 'store'])
+        ->name('submissions.store');
+
+    Route::get('classwork/{classwork}/file', [SubmissionController::class, 'file'])
+        ->name('submissions.file');
+});
+
+Route::post('/payments/stripe/webhook', StripeController::class);
